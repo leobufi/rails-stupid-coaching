897c5f6b38159eff33a51d8da48912067f388391
Bufi Leo <bufi.leo@gmail.com>
2022-02-14 12:59:35 +0100

    rails stupid coach ok

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @user_question = params[:question]
+
+    if @user_question == "I am going to work"
+      @coach_answer = "Great!"
+    elsif @user_question.include? "?"
+      @coach_answer = "Silly question, get dressed and go to work!"
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+end
